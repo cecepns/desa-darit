ddf3f11bf995d81e36fe268e8f987637cf5d291b
Cecep Nandang <cecep@kima.one>
2025-09-13 11:06:21 +0700

    fix: section

--- src/pages/landing/HomePage.jsx
+++ src/pages/landing/HomePage.jsx
@@ -276,7 +276,7 @@ const HomePage = () => {
       </section>
 
       {/* Map Section */}
-      <section>
+      <section className="pt-24">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="text-center mb-8" data-aos="fade-up">
             <h2 className="text-3xl lg:text-4xl font-bold text-gray-900 mb-3">
